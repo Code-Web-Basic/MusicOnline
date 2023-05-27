ea60ef742e004e965d7435baa20969eea24d196d
tumolaha <tu30380@gmail.com>
2023-05-27 16:36:55 +0700

    update show comment music

--- src/layout/Publish/PlayMusic/ShowCurrentMusic.js
+++ src/layout/Publish/PlayMusic/ShowCurrentMusic.js
@@ -1,33 +1,6 @@
-import { faker } from '@faker-js/faker';
-import {
-    Box,
-    Checkbox,
-    IconButton,
-    ListItemIcon,
-    MenuItem,
-    Paper,
-    Stack,
-    Tooltip,
-    Typography,
-    useTheme,
-} from '@mui/material';
-import Tippy from '@tippyjs/react/headless';
-import { DotsThree, Heart, Link as LinkIcon, PlusCircle, Share } from 'phosphor-react';
-
-const MENU_OPTIONS = [
-    {
-        label: 'Thêm vào playlist',
-        icon: <PlusCircle size={20} />,
-    },
-    {
-        label: 'Sao chép link',
-        icon: <LinkIcon size={20} />,
-    },
-    {
-        label: 'Chia sẽ',
-        icon: <Share size={20} />,
-    },
-];
+import { Box, Checkbox, Stack, Tooltip, Typography, useTheme } from '@mui/material';
+import { Heart } from 'phosphor-react';
+import { MoreButtonMusic } from '~/components/MoreButtonMusic/MoreButtonMusic';
 
 function ShowCurrentMusic({ data }) {
     const theme = useTheme();
@@ -69,7 +42,7 @@ function ShowCurrentMusic({ data }) {
                             checkedIcon={<Heart size={18} weight="fill" color={theme.palette.secondary.main} />}
                         />
                     </Tooltip>
-                    <MoreButton />
+                    <MoreButtonMusic />
                 </Stack>
             </Stack>
         </Stack>
@@ -77,92 +50,3 @@ function ShowCurrentMusic({ data }) {
 }
 
 export default ShowCurrentMusic;
-
-function MoreButton() {
-    const theme = useTheme();
-    return (
-        <>
-            <Tippy
-                interactive
-                trigger="click"
-                placement="top-end"
-                render={(attrs) => (
-                    <div className="box" tabIndex="-1" {...attrs}>
-                        <Paper sx={{ background: '#34224f', padding: '1px', minWidth: 200 }}>
-                            <Box sx={{ my: 1.5, px: 2.5 }}>
-                                <Stack direction={'row'} gap={'8px'} alignItems={'center'}>
-                                    <Box
-                                        sx={{
-                                            width: 32,
-                                            height: 32,
-                                            borderRadius: '10px',
-                                            overflow: 'hidden',
-                                        }}
-                                    >
-                                        <img
-                                            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                                            src={faker.image.avatarLegacy()}
-                                            alt="userui"
-                                        />
-                                    </Box>
-                                    <Stack direction={'column'} maxWidth={150} overflow={'hidden'}>
-                                        <Typography
-                                            variant="subtitle2"
-                                            fontSize={'0.8rem'}
-                                            noWrap
-                                            sx={{ color: theme.palette.grey[400] }}
-                                            textOverflow={'ellipsis'}
-                                        >
-                                            Dù Khóc Một Dòng Sông
-                                        </Typography>
-                                        <Typography
-                                            variant="body2"
-                                            fontSize={'0.5rem'}
-                                            sx={{ color: theme.palette.grey[400] }}
-                                            noWrap
-                                        >
-                                            Mai Tiến Dũng
-                                        </Typography>
-                                    </Stack>
-                                </Stack>
-                            </Box>
-                            <Stack sx={{ p: '4px' }}>
-                                {MENU_OPTIONS.map((option) => (
-                                    <MenuItem
-                                        key={option.label}
-                                        // onClick={handleClose}
-                                        sx={{ color: theme.palette.grey[400] }}
-                                    >
-                                        <ListItemIcon sx={{ color: theme.palette.grey[400] }}>
-                                            {option.icon}
-                                        </ListItemIcon>
-                                        {option.label}
-                                    </MenuItem>
-                                ))}
-                            </Stack>
-
-                            {/* <MenuItem onClick={handleLogout} sx={{ m: 1, color: theme.palette.grey[400] }}>
-                            <ListItemIcon sx={{ m: 1, color: theme.palette.grey[400] }}>
-                                <SignOut size={20} weight="fill" />
-                            </ListItemIcon>
-                            Logout
-                        </MenuItem> */}
-                        </Paper>
-                    </div>
-                )}
-            >
-                <Tooltip title="khac">
-                    <IconButton
-                        sx={{
-                            '&:hover': {
-                                background: theme.palette.grey[800],
-                            },
-                        }}
-                    >
-                        <DotsThree size={18} weight="bold" color={theme.palette.common.white} />
-                    </IconButton>
-                </Tooltip>
-            </Tippy>
-        </>
-    );
-}
