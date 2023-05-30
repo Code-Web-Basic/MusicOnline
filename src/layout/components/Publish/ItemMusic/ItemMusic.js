b9e5df81a151a2795886dca78d5b9bc527490e2c
tumolaha <tu30380@gmail.com>
2023-05-31 00:30:54 +0700

    update bug

--- src/layout/components/Publish/ItemMusic/ItemMusic.js
+++ src/layout/components/Publish/ItemMusic/ItemMusic.js
@@ -58,7 +58,7 @@ function ItemMusic({ music, type = 'medium', data }) {
                     )}
                     <img
                         style={{ height: '100%', width: '100%', objectFit: 'cover' }}
-                        src={music?.data?.thumbnail ? music?.data?.thumbnail : images.noImageMusic}
+                        src={data?.thumbnail ? data?.thumbnail : images.noImageMusic}
                         alt="music"
                     />
                 </Box>
